946cb90fad36d3b6a8d591c17b7f287845a3017f
Julio Aguilar Carmona <julio.ac@ittux.edu.mx>
2022-03-01 09:48:00 -0600

    Unico commit

---
+++ .history/src/App_20220224091421.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>Mira mamá. mi primer hola mundo en ReactJS</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Saludo.jsx
@@ -0,0 +1,5 @@
+const Saludo = () => {
+  return <h1>Este es mi segundo Hola Mundo en ReactJS!!</h1>;
+};
+
+export default Saludo;
